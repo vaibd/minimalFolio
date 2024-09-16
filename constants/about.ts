94783ba86b9f013dfbb07bbb6af8a5e066e35645
Vaibhav Dalakoti <38762713+vaibd@users.noreply.github.com>
2024-09-16 19:54:46 +0530

    Update about.ts

--- constants/about.ts
+++ constants/about.ts
@@ -2,9 +2,9 @@ export const myName = "Vaibhav Dalakoti";
 
 export const hero = {
   heading: "Hi, I am Vaibhav.",
-  heading2: "A Front End Engineer.",
+  heading2: "A Front End & Full Stack Engineer.",
   content:
-    "As a front end engineer, my expertise lies in creating and designing outstanding digital interfaces.",
+    "As a developer, my expertise lies in creating and designing outstanding digital interfaces. Currently having ~3yrs of exp.",
 };
 
 export const about = {
